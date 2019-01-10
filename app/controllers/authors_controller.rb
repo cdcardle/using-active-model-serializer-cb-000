d91892cf129168e2f3993089c3749b54f3df3638
Chris Cardle <cdcardle@gmail.com>
2019-01-10 15:57:57 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,6 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
-    format.html { render :show }
     render json: @author, status: 200
   end
 end
